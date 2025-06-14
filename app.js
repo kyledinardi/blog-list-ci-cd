2b132c20f82707b1ea7dcff51f7425ee60cb51a8
kyled <94251418+kyledinardi@users.noreply.github.com>
2025-06-14 16:49:19 -0400

    Add dist to backend and add playwright server

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ mongoose.connect(MONGODB_URI);
 app.use(cors());
 app.use(express.json());
 app.use(middleware.tokenExtractor);
+app.use(express.static('frontend/dist'));
 
 app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5173',
+    baseURL: 'http://localhost:3003',
     
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -62,9 +62,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'cd ../ && npm run start:test',
+    url: 'http://127.0.0.1:3003',
+    reuseExistingServer: !process.env.CI,
+  },
 });
